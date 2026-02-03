42c26ba10b13c3964b546aced57ab44610f37af3
jsilva <joaovictordes02@gmail.com>
2026-02-03 19:20:15 -0300

    alteração do bg e ajuste de conteúdo pessoal

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import Teste from "./components/teste";
-import AnimatedBackground from "./components/AnimatedBackground";
+import PageHome from "./components/Home";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen font-sans">
       {/* Fundo animado global */}
-      <AnimatedBackground />
 
       {/* Conteúdo principal */}
-      <main className="relative z-10 flex min-h-screen w-full items-center justify-center p-4 md:p-8">
-        <Teste />
+      <main className="relative z-10 flex min-h-screen w-full items-center justify-center p-4 md:p-8 bg-[url('/bg.jpg')] bg-cover">
+        <PageHome />
       </main>
     </div>
   );
